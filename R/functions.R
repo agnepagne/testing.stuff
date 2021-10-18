9cedb0bf1f8266653c8a5e705711db6734381060
Agne <agnel@kth.se>
2021-10-18 17:57:48 +0200

    Add imports referencing networkD3

--- R/functions.R
+++ R/functions.R
@@ -1,8 +1,23 @@
 #' Just a dummy function
 #'
 #' @param x a value to add to y
-#' @param y a valut to add to x
+#' @param y a value to add to x
 #' @return the sum of x and y
 #' @export
 
 sum2 <- function(x, y){x + y}
+
+#' Just another dummy function
+#'
+#' @param x a value
+#' @param y a value
+#' @return a data frame
+#' @importFrom networkD3 JS forceNetwork
+#' @export
+
+dummyfun <- function(x, y)
+{
+  data.frame(x = x, y = y)
+}
+
+
